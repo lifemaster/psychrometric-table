3ae6ece7b0c8dc200f376999456d4ec6cbf630f6
lifemaster <kostya.shyika@gmail.com>
2020-11-11 11:31:44 +0200

    Add project files.

---
+++ PsychrometricTable/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿using System.Windows.Forms;
+
+namespace PsychrometricTable
+{
+    class ComboItem
+    {
+        public string DryThermometerValue { get; set; }
+        public int Value { get; set; }
+        public string Text { get; set; }
+    }
+
+    partial class PsychrometricTableForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PsychrometricTableForm));
+            this.labelOfDryThermometer = new System.Windows.Forms.Label();
+            this.labelOfWetThermometer = new System.Windows.Forms.Label();
+            this.dryThermometerCombobox = new System.Windows.Forms.ComboBox();
+            this.wetThermometerCombobox = new System.Windows.Forms.ComboBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.airPressureLabel = new System.Windows.Forms.Label();
+            this.airPressureCombobox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // labelOfDryThermometer
+            // 
+            this.labelOfDryThermometer.AutoSize = true;
+            this.labelOfDryThermometer.Location = new System.Drawing.Point(22, 19);
+            this.labelOfDryThermometer.Name = "labelOfDryThermometer";
+            this.labelOfDryThermometer.Size = new System.Drawing.Size(187, 13);
+            this.labelOfDryThermometer.TabIndex = 0;
+            this.labelOfDryThermometer.Text = "Показание сухого термометра (°C):";
+            // 
+            // labelOfWetThermometer
+            // 
+            this.labelOfWetThermometer.AutoSize = true;
+            this.labelOfWetThermometer.Location = new System.Drawing.Point(22, 55);
+            this.labelOfWetThermometer.Name = "labelOfWetThermometer";
+            this.labelOfWetThermometer.Size = new System.Drawing.Size(203, 13);
+            this.labelOfWetThermometer.TabIndex = 1;
+            this.labelOfWetThermometer.Text = "Показание влажного термометра (°C):";
+            // 
+            // dryThermometerCombobox
+            // 
+            this.dryThermometerCombobox.FormattingEnabled = true;
+            this.dryThermometerCombobox.Location = new System.Drawing.Point(231, 16);
+            this.dryThermometerCombobox.Name = "dryThermometerCombobox";
+            this.dryThermometerCombobox.Size = new System.Drawing.Size(70, 21);
+            this.dryThermometerCombobox.TabIndex = 2;
+            this.dryThermometerCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.dryThermometerCombobox.SelectedIndexChanged += new System.EventHandler(this.DryThermometerCombobox_SelectedIndexChanged);
+            // 
+            // wetThermometerCombobox
+            // 
+            this.wetThermometerCombobox.FormattingEnabled = true;
+            this.wetThermometerCombobox.Location = new System.Drawing.Point(231, 52);
+            this.wetThermometerCombobox.Name = "wetThermometerCombobox";
+            this.wetThermometerCombobox.Size = new System.Drawing.Size(70, 21);
+            this.wetThermometerCombobox.TabIndex = 3;
+            this.wetThermometerCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(118, 130);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(100, 23);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Рассчитать";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // airPressureLabel
+            // 
+            this.airPressureLabel.AutoSize = true;
+            this.airPressureLabel.Location = new System.Drawing.Point(22, 88);
+            this.airPressureLabel.Name = "airPressureLabel";
+            this.airPressureLabel.Size = new System.Drawing.Size(166, 13);
+            this.airPressureLabel.TabIndex = 5;
+            this.airPressureLabel.Text = "Давление воздуха (мм. рт. ст.):";
+            // 
+            // airPressureCombobox
+            // 
+            this.airPressureCombobox.FormattingEnabled = true;
+            this.airPressureCombobox.Location = new System.Drawing.Point(231, 85);
+            this.airPressureCombobox.Name = "airPressureCombobox";
+            this.airPressureCombobox.Size = new System.Drawing.Size(70, 21);
+            this.airPressureCombobox.TabIndex = 6;
+            this.airPressureCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
+            // 
+            // PsychrometricTableForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 171);
+            this.Controls.Add(this.airPressureCombobox);
+            this.Controls.Add(this.airPressureLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.wetThermometerCombobox);
+            this.Controls.Add(this.dryThermometerCombobox);
+            this.Controls.Add(this.labelOfWetThermometer);
+            this.Controls.Add(this.labelOfDryThermometer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PsychrometricTableForm";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Психрометрическая таблица";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelOfDryThermometer;
+        private System.Windows.Forms.Label labelOfWetThermometer;
+        private System.Windows.Forms.ComboBox dryThermometerCombobox;
+        private System.Windows.Forms.ComboBox wetThermometerCombobox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label airPressureLabel;
+        private System.Windows.Forms.ComboBox airPressureCombobox;
+    }
+}
+
---
+++ PsychrometricTable/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PsychrometricTable
+{
+    public partial class PsychrometricTableForm : Form
+    {
+        private Dictionary<int, double> SaturatedWaterVaporElasticityDictionary = new Dictionary<int, double>
+        {
+            { -10, 2.862 }, { -9, 3.097 }, { -8, 3.348 }, { -7, 3.617 }, { -6, 3.906 },
+            { -5, 4.214 }, { -4, 4.544 }, { -3, 4.897 }, { -2, 5.275 }, { -1, 5.677 }, { 0, 6.107 },
+            { 1, 6.565 }, { 2, 7.054 }, { 3, 7.574 }, { 4, 8.128 }, { 5, 8.718 },
+            { 6, 9.345 }, { 7, 10.012 }, { 8, 10.720 }, { 9, 11.472 }, { 10, 12.771 },
+            { 11, 13.118 }, { 12, 14.015 }, { 13, 14.967 }, { 14, 15.975 }, { 15, 17.042 },
+            { 16, 18.171 }, { 17, 19.365 }, { 18, 20.628 }, { 19, 21.962 }, { 20, 23.371 },
+            { 21, 24.858 }, { 22, 26.428 }, { 23, 28.083 }, { 24, 29.829 }, { 25, 31.668 },
+            { 26, 33.606 }, { 27, 35.646 }, { 28, 37.793 }, { 29, 40.052 }, { 30, 42.427 },
+            { 31, 44.924 }, { 32, 47.548 }, { 33, 50.303 }, { 34, 53.196 }, { 35, 56.233 },
+            { 36, 59.418 }, { 37, 62.758 }, { 38, 66.260 }, { 39, 69.930 }, { 40, 73.773 }
+        };
+
+        public PsychrometricTableForm()
+        {
+            InitializeComponent();
+
+            InitializeDryThermometerComboboxDataSource();
+            InitializeWetThermometerComboboxDataSource();
+            InitializeAirPressureComboboxDataSource();
+        }
+
+        private void InitializeDryThermometerComboboxDataSource()
+        {
+            List<ComboItem> dryThermometerComboboxDataSource = new List<ComboItem> { };
+
+            for (int i = 0; i <= 40; i++)
+            {
+                dryThermometerComboboxDataSource.Add(new ComboItem { Value = i, Text = i.ToString() });
+            }
+
+            dryThermometerCombobox.DataSource = dryThermometerComboboxDataSource;
+            dryThermometerCombobox.SelectedIndex = 20;
+            dryThermometerCombobox.DisplayMember = "Text";
+            dryThermometerCombobox.ValueMember = "Value";
+        }
+
+        private void InitializeWetThermometerComboboxDataSource()
+        {
+            List<ComboItem> wetThermometerComboboxDataSource = new List<ComboItem> { };
+            int dryThermometerValue = Int32.Parse(dryThermometerCombobox.SelectedValue.ToString());
+
+            for (int i = dryThermometerValue; i >= dryThermometerValue - 10; i--)
+            {
+                wetThermometerComboboxDataSource.Add(new ComboItem { Value = i, Text = i.ToString() });
+            }
+
+            wetThermometerCombobox.DataSource = wetThermometerComboboxDataSource;
+            wetThermometerCombobox.DisplayMember = "Text";
+            wetThermometerCombobox.ValueMember = "Value";
+        }
+
+        private void InitializeAirPressureComboboxDataSource()
+        {
+            List<ComboItem> airPressureComboboxDataSource = new List<ComboItem> { };
+
+            for (int i = 720; i <=790; i++)
+            {
+                airPressureComboboxDataSource.Add(new ComboItem { Value = i, Text = i.ToString() });
+            }
+
+            airPressureCombobox.DataSource = airPressureComboboxDataSource;
+            airPressureCombobox.SelectedIndex = 40;
+            airPressureCombobox.DisplayMember = "Text";
+            airPressureCombobox.ValueMember = "Value";
+        }
+
+        private void DryThermometerCombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!Int32.TryParse(dryThermometerCombobox.SelectedValue.ToString(), out int dryThermometerTemperature))
+            {
+                return;
+            }
+
+            List<ComboItem> wetThermometerComboboxDataSource = new List<ComboItem> { };
+
+            for (int i = dryThermometerTemperature; i >= (dryThermometerTemperature - 10); i--)
+            {
+                wetThermometerComboboxDataSource.Add(new ComboItem { Value = i, Text = i.ToString() });
+            }
+
+            wetThermometerCombobox.DataSource = wetThermometerComboboxDataSource;
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            int dryTemperature = Int32.Parse(dryThermometerCombobox.SelectedValue.ToString());
+            int wetTemperature = Int32.Parse(wetThermometerCombobox.SelectedValue.ToString());
+            double airPressure = Int32.Parse(airPressureCombobox.SelectedValue.ToString());
+
+            double saturatedWaterVaporElasticityForWetThermometerValue = this.SaturatedWaterVaporElasticityDictionary[wetTemperature];
+            double saturatedWaterVaporElasticityForDryThermometerValue = this.SaturatedWaterVaporElasticityDictionary[dryTemperature];
+
+            double waterVaporPressure = saturatedWaterVaporElasticityForWetThermometerValue - 7.947e-4 * (dryTemperature - wetTemperature) * airPressure * 1.33322;
+
+            double relativeHumidity = (waterVaporPressure / saturatedWaterVaporElasticityForDryThermometerValue) * 100;
+            double roudedRelativeHumidity = Math.Round(relativeHumidity * 100) / 100;
+
+            string resultText = $"Относительная влажность воздуха: {roudedRelativeHumidity} %";
+
+            MessageBox.Show(resultText, "Результаты расчета", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
